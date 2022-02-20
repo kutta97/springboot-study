ff98782819187f89ef8b4085e45b9ec2b2cfb543
kutta97 <yhjin04170@gmail.com>
2022-02-20 17:51:47 +0900

    필요없는 import 제거

--- src/main/java/springboot/domain/posts/PostsRepository.java
+++ src/main/java/springboot/domain/posts/PostsRepository.java
@@ -1,7 +1,6 @@
 package springboot.domain.posts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import springboot.domain.posts.Posts;
 
 public interface PostsRepository extends JpaRepository<Posts, Long> {
 }
