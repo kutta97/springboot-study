02d8d02f14b7f58d9d62776e3ccf547a048763ae
kutta97 <yhjin04170@gmail.com>
2022-02-20 21:08:30 +0900

    Post 등록 Api 작성

---
+++ src/main/java/springboot/web/PostsApiController.java
@@ -0,0 +1,18 @@
+package springboot.web;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+import springboot.service.posts.PostsService;
+import springboot.web.dto.PostsSaveRequestDto;
+
+@RequiredArgsConstructor
+@RestController
+public class PostsApiController {
+    private final PostsService postsService;
+
+    @PostMapping("/api/v1/posts")
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsService.save(requestDto);
+    }
+}
